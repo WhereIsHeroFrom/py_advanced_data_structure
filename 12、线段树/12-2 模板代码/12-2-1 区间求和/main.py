c8d1d5fa2dd87f2e990764a81dc4fde79d6cedf4
WhereIsHeroFrom <menjitianya2007@163.com>
2026-05-07 13:34:02 +0800

    题目提交完成

---
+++ 12、线段树/12-2 模板代码/12-2-1 区间求和/main.py
@@ -0,0 +1,90 @@
+##########################线段树模板(区间求和)##########################
+class SegmentTree:
+    def __init__(self, n, arr):
+        self.n = n
+        self.arr = arr
+        self.tree = [0] * (n * 4)
+        self.lazy = [0] * (n * 4)
+        self.build(0, 0, n-1)
+
+    def build(self, node, start, end):
+        if start == end:
+            self.tree[node] = self.arr[start]
+            return 
+        mid = (start + end) // 2
+        left_child = node*2 + 1
+        right_child = left_child + 1
+        self.build(left_child, start, mid)
+        self.build(right_child, mid + 1, end)
+        self.tree[node] = self.tree[left_child] + self.tree[right_child]
+
+    def pushDown(self, node, start, end):
+        if self.lazy[node] != 0:
+            mid = (start + end) // 2
+            left_child = node*2 + 1
+            right_child = left_child + 1
+            self.tree[left_child] += self.lazy[node] * (mid - start + 1)
+            self.tree[right_child] += self.lazy[node] * (end - mid)
+
+            self.lazy[left_child] += self.lazy[node]
+            self.lazy[right_child] +=  self.lazy[node]
+
+            self.lazy[node] = 0
+    
+    def updateRange(self, node, start, end, l, r, val):
+        if start > r or end < l:
+            return 
+        # l    start   end    r
+        if start >= l and end <= r:
+            self.tree[node] += val * (end - start + 1)
+            self.lazy[node] += val
+            return 
+        self.pushDown(node, start, end)
+        mid = (start + end) // 2
+        left_child = node*2 + 1
+        right_child = left_child + 1
+        self.updateRange(left_child, start, mid, l, r, val)
+        self.updateRange(right_child, mid+1, end, l, r, val)
+        self.tree[node] = self.tree[left_child] + self.tree[right_child]
+
+    def queryRange(self, node, start, end, l, r):
+        if start > r or end < l:
+            return 0
+        if start >= l and end <= r:
+            return self.tree[node]
+        self.pushDown(node, start, end)
+        mid = (start + end) // 2
+        left_child = node*2 + 1
+        right_child = left_child + 1
+        lsum = self.queryRange(left_child, start, mid, l, r)
+        rsum = self.queryRange(right_child, mid+1, end, l, r)
+        return lsum + rsum
+    
+    def Update(self, l, r, val):
+        self.updateRange(0, 0, n-1, l, r, val)
+
+    def Query(self, l, r):
+        return self.queryRange(0, 0, n-1, l, r)
+
+##########################线段树模板(区间求和)##########################
+
+import sys
+data = iter(sys.stdin.read().split())
+
+n = int(next(data))
+m = int(next(data))
+a = [int(next(data)) for _ in range(n)]
+seg = SegmentTree(n, a)
+
+out = []
+while m > 0:
+    m -= 1
+    z = int(next(data))
+    x = int(next(data)) - 1
+    y = int(next(data)) - 1
+    if z == 1:
+        seg.Update(x, y, int(next(data)))
+    else:
+        out.append( str(seg.Query(x, y)) )
+
+sys.stdout.write("\n".join(out) + '\n')
